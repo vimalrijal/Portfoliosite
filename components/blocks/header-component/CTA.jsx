cf1e05fa804b7362b3bc3e9881f885491f268cf8
vimal rijal <106002509+vimalrijal@users.noreply.github.com>
2023-02-05 19:49:14 +1100

    Add files via upload

---
+++ components/blocks/header-component/CTA.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import CV from "../../../public/assets/VimalRijal";
+const CTA = () => {
+  return (
+    <div className="cta">
+      {/* <a href={CV} download className="btn" target="_blank" rel="noreferrer">
+        Download CV
+      </a> */}
+      <a href="#contact" className="btn btn-primary">
+        Lets Talk
+      </a>
+    </div>
+  );
+};
+
+export default CTA;
